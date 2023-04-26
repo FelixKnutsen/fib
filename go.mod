e16db41f1ccd74d4c7c64049ae52a5e64206e71a
Felix <felixfk@uia.no>
2023-04-26 12:36:38 +0200

    dobbelsjekket at alt fungerte og la til bevis at koden fungerer

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/FelixKnutsen/fib
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+import "github.com/FelixKnutsen/fib/fib"
+
+
+func main() {
+	fmt.Println("Fantastiske Fibonacci!")
+fmt.Println(fib.Fib(10))
+}
